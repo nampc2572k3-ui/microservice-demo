61c049503ea37f8b917dfad774654534773ff714
Phạm Quang Nam <nampc2572k3@gmail.com>
2026-03-05 16:13:24 +0700

    feature/init profile-service

---
+++ profile-service/src/main/java/com/example/demo/core/config/db/Neo4jConfig.java
@@ -0,0 +1,11 @@
+package com.example.demo.core.config.db;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.neo4j.config.EnableNeo4jAuditing;
+
+@Configuration
+@EnableNeo4jAuditing
+public class Neo4jConfig {
+
+    // todo
+}
---
+++ profile-service/src/main/java/com/example/demo/profile/repository/UserProfileRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.profile.repository;
+
+import com.example.demo.profile.model.entity.UserProfile;
+import org.springframework.data.neo4j.repository.Neo4jRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserProfileRepository extends Neo4jRepository<UserProfile, UUID> {
+}
