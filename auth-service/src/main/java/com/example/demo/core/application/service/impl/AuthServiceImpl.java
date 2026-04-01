735b74eb5d9a97ddde3dad914a7135fdb17aaad8
Phạm Quang Nam <nampc2572k3@gmail.com>
2026-04-01 17:40:39 +0700

    feature/update login

--- auth-service/src/main/java/com/example/demo/core/application/service/impl/AuthServiceImpl.java
+++ auth-service/src/main/java/com/example/demo/core/application/service/impl/AuthServiceImpl.java
@@ -3,17 +3,15 @@ package com.example.demo.core.application.service.impl;
 import com.example.demo.common.constant.ErrorCode;
 import com.example.demo.common.exception.CustomBusinessException;
 import com.example.demo.core.adapter.http.PermissionClient;
+import com.example.demo.core.application.cache.LoginRateLimitCache;
 import com.example.demo.core.application.dto.request.LoginRequest;
 import com.example.demo.core.application.dto.request.RefreshTokenRequest;
 import com.example.demo.core.application.dto.request.RegisterRequest;
 import com.example.demo.core.application.dto.response.LoginResponse;
 import com.example.demo.core.application.dto.response.RefreshTokenResponse;
 import com.example.demo.core.application.service.AuthService;
-import com.example.demo.core.application.cache.LoginRateLimitCache;
-import com.example.demo.core.domain.event.internal.LoginFailedEvent;
 import com.example.demo.core.domain.event.internal.LoginSuccessTransactionalEvent;
 import com.example.demo.core.domain.helper.AuthHelper;
-import com.example.demo.core.domain.helper.RefreshTokenHelper;
 import com.example.demo.core.domain.model.entity.Account;
 import com.example.demo.core.domain.model.entity.AccountDevice;
 import com.example.demo.core.persistence.AccountRepository;
@@ -32,7 +30,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -134,14 +131,12 @@ public class AuthServiceImpl implements AuthService {
                     .build();
 
         } catch (Exception e) {
-            eventPublisher.publishEvent(
-                  new LoginFailedEvent(request.getIdentity(), clientIp, e.getMessage())
-            );
+            authHelper.recordLoginAttempt(request.getIdentity(), clientIp, false, e.getMessage());
 
             log.error(e.getMessage());
 
             throw new CustomBusinessException(
-                    ErrorCode.LOGIN_FAIL.getCode(),
+                    ErrorCode.BAD_CREDENTIALS.getCode(),
                     e.getMessage()
             );
         }
